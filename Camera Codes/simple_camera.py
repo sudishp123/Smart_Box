21a6582f366b76f684e17f05462a07af34627218
sudishp123 <60830186+sudishp123@users.noreply.github.com>
2025-08-06 11:12:03 -0300

    Update simple_camera.py

--- Camera Codes/simple_camera.py
+++ Camera Codes/simple_camera.py
@@ -1,6 +1,3 @@
-# MIT License
-# Copyright (c) 2019-2022 JetsonHacks
-
 # Using a CSI camera (such as the Raspberry Pi Version 2) connected to a
 # NVIDIA Jetson Nano Developer Kit using OpenCV
 # Drivers for the camera and OpenCV are included in the base image
